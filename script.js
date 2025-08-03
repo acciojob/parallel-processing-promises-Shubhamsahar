556180219783f40a825088a4e6e40ea3bc274ebf
Shubhamsahar <kumarshubham765484@gmail.com>
2025-08-03 18:09:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-//your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-
-const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
 function loadImage(url) {
     return new Promise((resolve, reject) => {
         const img = new Image();
@@ -33,3 +24,4 @@ btn.addEventListener("click", () => {
             output.appendChild(errorDiv);
         });
 });
+
