4353ea65a2705776410f07e8e8ddaf417cd3001f
Shubhamsahar <kumarshubham765484@gmail.com>
2025-08-03 18:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function loadImage(url) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = () => reject(`Failed to load image from ${url}`);
+        img.src = url;
+    });
+}
+
+btn.addEventListener("click", () => {
+    loadingDiv.style.display = "block";
+    errorDiv.innerText = "";
+    output.innerHTML = "";
+
+    const promises = images.map((image) => loadImage(image.url));
+    Promise.all(promises)
+        .then((imgs) => {
+            loadingDiv.style.display = "none";
+            imgs.forEach((img) => output.appendChild(img));
+        })
+        .catch((error) => {
+            loadingDiv.style.display = "none";
+            errorDiv.innerText = error;
+            output.appendChild(errorDiv);
+        });
+});
